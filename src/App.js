db6d23af0fc117b19fc1183b2fcaad1541103306
Alex Bajaña <alexb.inf.4891@gmail.com>
2023-11-06 18:56:19 -0500

    Cambio de la App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
+/*Vamos a crear una App con el titulo de nivel 1, luego un input de texto y por ultimo un boton para desencadenar una acción*/
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  return (
+    <div>
+      <h1>Titulo de nivel 1</h1>
+      <input type="text" />
+      <button>Enviar</button>
+    </div>
+  );
 }
 
 export default App;
