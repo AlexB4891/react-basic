0d0016ca8349b7e23c3cf280b4ce93da7283fa82
Alex Bajaña <alexb.inf.4891@gmail.com>
2023-11-06 20:24:24 -0500

    Definidas las variables que faltaban

--- src/App.js
+++ src/App.js
@@ -1,10 +1,4 @@
-import React, { useEffect, useState } from 'react';
-
-/*Vamos a crear una App con el titulo de nivel 1, luego un input de texto y por ultimo un boton para desencadenar una acción*/
-/* link the app with the css file*/
-/* add an instruction for the input*/
-
-
+import React, { useState, useEffect } from 'react';
 import { analyzeImage } from './azure-image-analysis.js';
 
 function App() {
@@ -13,25 +7,31 @@ function App() {
   const [imageSrc, setImageSrc] = useState('');
   const [jsonResponse, setJsonResponse] = useState('');
 
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }, []);
+
   const handleButtonClick = () => {
     analyzeImage(imageUrl).then((result) => {
-      /*setImageDescription(result.description);*/
+      setImageDescription(result.description);
       setImageSrc(result.imageUrl);
-      /*setJsonResponse(JSON.stringify(result.jsonResponse, null, 2));*/
+      setJsonResponse(JSON.stringify(result.jsonResponse, null, 2));
     });
   };
 
   return (
     <div>
-      <link rel="stylesheet" href="./index.css" />
       <h1>Aplicación para describir/generar imagenes</h1>
       <p>Aquí abajo puedes escribir tu prompt para generar imagenes o en su defecto puedes pegar un URL y la AI de Azure va a describir lo que está dentro de tu imagen</p>
-      <input  type="text" placeholder="Escribe tu URL o prompt aquí" value={imageUrl} onChange={(e) => setImageUrl(e.target.value)} />
-      <button onClick={handleButtonClick}>Enviar</button> 
-      <img src={imageSrc} alt="Imagen analizada" />
+      <input type="text" placeholder="Escribe tu URL o prompt aquí" value={imageUrl} onChange={(e) => setImageUrl(e.target.value)} />
+      <button onClick={handleButtonClick}>Enviar</button>
+      <p>{imageDescription}</p>
+      {imageSrc && <img src={imageSrc} alt="Imagen analizada" />}
+      <pre>{jsonResponse}</pre>
     </div>
   );
 }
-
 
 export default App;
