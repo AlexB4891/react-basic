8686f09b292633461895e89cb883bd2e7eb30c4e
Alex Bajaña <alexb.inf.4891@gmail.com>
2023-11-06 20:48:02 -0500

    Solo muestra la imagen

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { analyzeImage } from './azure-image-analysis.js';
 
 function App() {
   const [imageUrl, setImageUrl] = useState('');
-  const [imageDescription, setImageDescription] = useState('');
-  const [imageSrc, setImageSrc] = useState('');
-  const [jsonResponse, setJsonResponse] = useState('');
-
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/todos/1')
-      .then((response) => response.json())
-      .then((data) => console.log(data));
-  }, []);
 
   const handleButtonClick = () => {
     analyzeImage(imageUrl).then((result) => {
-      setImageDescription(result.description);
-      setImageSrc(result.imageUrl);
-      setJsonResponse(JSON.stringify(result.jsonResponse, null, 2));
+      setImageUrl(result.imageUrl);
     });
   };
 
@@ -27,9 +16,7 @@ function App() {
       <p>Aquí abajo puedes escribir tu prompt para generar imagenes o en su defecto puedes pegar un URL y la AI de Azure va a describir lo que está dentro de tu imagen</p>
       <input type="text" placeholder="Escribe tu URL o prompt aquí" value={imageUrl} onChange={(e) => setImageUrl(e.target.value)} />
       <button onClick={handleButtonClick}>Enviar</button>
-      <p>{imageDescription}</p>
-      {imageSrc && <img src={imageSrc} alt="Imagen analizada" />}
-      <pre>{jsonResponse}</pre>
+      {imageUrl && <img src={imageUrl} alt="Imagen analizada" />}
     </div>
   );
 }
