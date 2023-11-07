5d2c4de33902bfa4ab86e486cafcb74e29ad5e16
Alex Bajaña <58230734+AlexB4891@users.noreply.github.com>
2023-11-06 20:19:15 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 /*Vamos a crear una App con el titulo de nivel 1, luego un input de texto y por ultimo un boton para desencadenar una acción*/
 /* link the app with the css file*/
