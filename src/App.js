a2ce5b61147ab14a431ab06e6de214cf2ede869c
Alex Bajaña <alexb.inf.4891@gmail.com>
2023-11-06 20:05:25 -0500

    corregido el useState

--- src/App.js
+++ src/App.js
@@ -8,33 +8,7 @@ import React from 'react';
 
 import { analyzeImage } from './azure-image-analysis.js';
 
-function App() {
-  const [imageUrl, setImageUrl] = useState('');
-  const [imageDescription, setImageDescription] = useState('');
-  const [imageSrc, setImageSrc] = useState('');
-  const [jsonResponse, setJsonResponse] = useState('');
-
-  const handleButtonClick = () => {
-    analyzeImage(imageUrl).then((result) => {
-      setImageDescription(result.description);
-      setImageSrc(result.imageUrl);
-      setJsonResponse(JSON.stringify(result.jsonResponse, null, 2));
-    });
-  };
-
-  return (
-    <div>
-      <link rel="stylesheet" href="./index.css" />
-      <h1>Aplicación para describir/generar imagenes</h1>
-      <p>Aquí abajo puedes escribir tu prompt para generar imagenes o en su defecto puedes pegar un URL y la AI de Azure va a describir lo que está dentro de tu imagen</p>
-      <input  type="text" placeholder="Escribe tu URL o prompt aquí" value={imageUrl} onChange={(e) => setImageUrl(e.target.value)} />
-      <button onClick={handleButtonClick}>Enviar</button> 
-      <p>{imageDescription}</p>
-      {imageSrc && <img src={imageSrc} alt="Imagen analizada" />}
-      <pre>{jsonResponse}</pre>
-    </div>
-  );
-}
+import React, { useState } from 'react';
 
 
 export default App;
