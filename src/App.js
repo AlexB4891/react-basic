7c0d5408df0c1fc41337fdb59be1b32fc6bfb9e5
Alex Bajaña <alexb.inf.4891@gmail.com>
2023-11-06 22:21:47 -0500

    Quitado el loading

--- src/App.js
+++ src/App.js
@@ -4,19 +4,15 @@ import { analyzeImage } from './azure-image-analysis.js';
 function App() {
   const [imageUrl, setImageUrl] = useState('');
   const [imageDescription, setImageDescription] = useState('');
-  const [loading, setLoading] = useState(false);
 
   const handleButtonClick = () => {
-    setLoading(true);
     analyzeImage(imageUrl).then((result) => {
-      setLoading(false);
       if (result && result.description && result.description.captions && result.description.captions.length > 0) {
         setImageDescription(result.description.captions[0].text);
       } else {
         setImageDescription('No se pudo obtener una descripción de la imagen.');
       }
-    }).catch((error) => {
-      setLoading(false);
+    }).catch(() => {
       setImageDescription('No se pudo obtener una descripción de la imagen.');
     });
   };
@@ -28,9 +24,8 @@ function App() {
       <div style={{ maxWidth: '600px' }}>
         <input type="text" placeholder="Escribe tu URL o prompt aquí" value={imageUrl} onChange={(e) => setImageUrl(e.target.value)} />
         {imageUrl && <img src={imageUrl} alt="Imagen analizada" style={{ height: '200px', width: 'auto' }} />}
-        {loading && <p>Cargando...</p>}
         {imageDescription && <p>{imageDescription}</p>}
-        {!loading && !imageDescription && <p>No se pudo obtener una descripción de la imagen.</p>}
+        {!imageDescription && <p>No se pudo obtener una descripción de la imagen.</p>}
       </div>
       <button onClick={handleButtonClick}>Enviar</button>
     </div>
