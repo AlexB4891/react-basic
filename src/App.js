07fa7d3febc452b3db1ce8e359cf86db9294581c
Alex Bajaña <alexb.inf.4891@gmail.com>
2023-11-06 20:59:25 -0500

    Incluidas las funcionalidades de Azure nuevamente

--- src/App.js
+++ src/App.js
@@ -3,10 +3,21 @@ import { analyzeImage } from './azure-image-analysis.js';
 
 function App() {
   const [imageUrl, setImageUrl] = useState('');
+  const [imageDescription, setImageDescription] = useState('');
+  const [loading, setLoading] = useState(false);
 
   const handleButtonClick = () => {
+    setLoading(true);
     analyzeImage(imageUrl).then((result) => {
-      setImageUrl(result.imageUrl);
+      setLoading(false);
+      if (result) {
+        setImageDescription(JSON.stringify(result, null, 2));
+      } else {
+        setImageDescription('Problema con Azure');
+      }
+    }).catch((error) => {
+      setLoading(false);
+      setImageDescription('Problema con Azure');
     });
   };
 
@@ -14,9 +25,18 @@ function App() {
     <div>
       <h1>Aplicación para describir/generar imagenes</h1>
       <p>Aquí abajo puedes escribir tu prompt para generar imagenes o en su defecto puedes pegar un URL y la AI de Azure va a describir lo que está dentro de tu imagen</p>
-      <input type="text" placeholder="Escribe tu URL o prompt aquí" value={imageUrl} onChange={(e) => setImageUrl(e.target.value)} />
+      <div style={{ display: 'flex', flexDirection: 'column' }}>
+        <input type="text" placeholder="Escribe tu URL o prompt aquí" value={imageUrl} onChange={(e) => setImageUrl(e.target.value)} />
+        {imageUrl && <img src={imageUrl} alt="Imagen analizada" style={{ height: '200px', width: 'auto' }} />}
+        {imageDescription && <pre>{imageDescription}</pre>}
+      </div>
       <button onClick={handleButtonClick}>Enviar</button>
-      {imageUrl && <img src={imageUrl} alt="Imagen analizada" />}
+    </div>
+  );
+}
+
+export default App;
+      
     </div>
   );
 }
